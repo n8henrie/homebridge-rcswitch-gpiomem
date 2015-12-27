baddc62b09ea68ee3271ee1550185841d7f8db7a
Nathan Henrie <nate@n8henrie.com>
2015-12-27 16:20:11 -0700

    Initial commit after fork.
    Major changes from https://github.com/FWeinb/homebridge-rcswitch:
    
    - Uses
      [rcswitch-gpiomem](https://github.com/n8henrie/node-rcswitch-gpiomem)
    - Updated config info and README
    - Accepts new config variables for use with rcswitch-gpiomem
    - Checks to ensure either both on and off codes or both systemcode and
      unitcode are set, sets defaults for pin, pulse length, and bit length
      if needed

--- README.md
+++ README.md
@@ -1,27 +1,39 @@
-# homebridge-rcswitch
+# homebridge-rcswitch-gpiomem
 
-Add support for rc switches using [rcswitch](https://github.com/marvinroger/node-rcswitch)
+<http://n8henrie.com/2015/12/control-an-rf-outlet-with-siri-via-homebridge>
+
+Control RF outlets with HomeKit / Siri using
+[rcswitch-gpiomem](https://github.com/n8henrie/node-rcswitch-gpiomem)
 
 # Installation
 
 1. Install [WiringPi](https://projects.drogon.net/raspberry-pi/wiringpi/download-and-install/)
-2. Install homebridge using: `npm install -g homebridge`
-3. Install this plugin using: `npm install -g homebridge-rcswitch`
-4. Update your configuration file.
+1. Install homebridge: `npm install -g homebridge`
+1. Install homebridge-rcswitch-gpiomem: `npm install --global
+   homebridge-rcswitch-gpiomem`
+1. Update your configuration file.
 
 # Configuration
 
-You can add as many switches as you like. You will need to pass the `name`, `systemcode` and `unitcode`.
-This plugin assumes that you connect the 433Mhz transmitter to GPIO0, this can be changed via the `pin` propertie. 
+See `sample-config.json`
 
- ```
- "accessories": [
-     {
-       "accessory": "RcSwitch",
-       "name": "Switch One",
-       "systemcode": "11101", 
-       "unitcode": 1,
-       "pin": 0
-     }
-]
-```
+Either `onCode` and `offCode` **or** `systemcode` and `unitcode` are required.
+My switches use on and off codes, so that's what I recommend and all I can
+really help with.
+
+- `accessory`: Must be `RCSwitch` (case sensitive)
+- `name` :: string :: What you want to call the switch. Keep in mind
+  that Siri will prefer anything other than your homebridge switch if there's
+  any confusion, so name it something unique
+- `onCode`, `offCode` :: int or string
+    - If int: Decimal RF code to turn switch on / off
+    - If string: Binary RF code to turn switch on / off
+- `systemcode` :: string :: RF system code. I don't use this, please
+  refer to other docs.
+- `unitcode` ::  int :: RF unit code. I don't use this, please refer
+  to other docs.
+- `pin` :: int, optional :: BCM pin connected to 433 mhz transmitter, defaults
+  to `17`
+- `pulseLength` :: int, optional :: RF pulse length, defaults to `190`
+- `bitLength` :: int, optional :: bit length of RF code, only used if using
+  decimal RF code, defaults to `24`
--- index.js
+++ index.js
@@ -1,65 +1,81 @@
-var rcswitch = require('rcswitch');
+var rcswitch = require('rcswitch-gpiomem');
 
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-rcswitch", "RcSwitch", RadioSwitch);
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory("homebridge-rcswitch-gpiomem", "RCSwitch", RadioSwitch);
 }
 
 function RadioSwitch(log, config) {
 
-  if (config.systemcode === undefined) {
-    return log("Systemcode missing from configuration.");
-  }
-  if (config.unitcode  === undefined) {
-    return log("Unitcode missing from configuration.");
-  }
-  if (config.name  === undefined) {
-    return log("Name missing from configuration.");
-  }
-
-  rcswitch.enableTransmit(config.pin || 0);
-
-  var switchOn = rcswitch.switchOn.bind(rcswitch, config.systemcode, config.unitcode);
-  var switchOff = rcswitch.switchOff.bind(rcswitch, config.systemcode, config.unitcode);
-
-  var informationService = new Service.AccessoryInformation();
-
-  informationService
-    .setCharacteristic(Characteristic.Name, "Raspberry-Projekt")
-    .setCharacteristic(Characteristic.Manufacturer, "JadeHochschule")
-    .setCharacteristic(Characteristic.Model, "v0.1")
-    .setCharacteristic(Characteristic.SerialNumber, "0000000001");
-
-  var state = false;
-  var switchService = new Service.Switch(config.name);
-
-  switchService
-  .getCharacteristic(Characteristic.On)
-  .on('set', function(value, callback) {
-    state = value;
-    if (state) {
-      switchOn();
-    } else {
-      switchOff();
+    if (config.name  === undefined) {
+        return log("Name missing from configuration.");
     }
-    callback();
-  });
 
-  switchService
-  .getCharacteristic(Characteristic.On)
-  .on('get', function(callback){
-    callback(null, state);
-  });
+    if ((config.onCode !== undefined) && (config.offCode !== undefined)) {
+        if (typeof(config.onCode) === "string") {
+            // If code is a string, it should be binary and don't need bit length
+            var switchOn = rcswitch.send.bind(rcswitch, config.onCode);
+        } else if (typeof(config.onCode) === "number") {
+            var bLength = config.bitLength || 24;
+            var switchOn = rcswitch.send.bind(rcswitch, config.onCode, bLength);
+        }
+        if (typeof(config.offCode) === "string") {
+            // If code is a string, it should be binary and don't need bit length
+            var switchOff = rcswitch.send.bind(rcswitch, config.offCode);
+        } else if (typeof(config.offCode) === "number") {
+            var bLength = config.bitLength || 24;
+            var switchOff = rcswitch.send.bind(rcswitch, config.offCode, bLength);
+        }
+    } else if ((config.systemcode != undefined) && (config.unitcode != undefined)) {
+        var switchOn = rcswitch.switchOn.bind(rcswitch, config.systemcode, config.unitcode);
+        var switchOff = rcswitch.switchOff.bind(rcswitch, config.systemcode, config.unitcode);
+    } else {
+        return log("Configuration must include either both an on code and an off code OR " +
+                "both a systemcode and a unitcode (or can have all of them), but " +
+                "doesn't appear to include at least one of these pairs.");
+    }
 
-   this.services = [ informationService, switchService ];
+    rcswitch.enableTransmit(config.pin || 17);
+    rcswitch.setPulseLength(config.pulseLength || 190);
+
+    var informationService = new Service.AccessoryInformation();
+
+    informationService
+        .setCharacteristic(Characteristic.Name, "node-rcswitch-gpiomem")
+        .setCharacteristic(Characteristic.Manufacturer, "n8henrie")
+        .setCharacteristic(Characteristic.Model, "v1.1.0")
+        .setCharacteristic(Characteristic.SerialNumber, "0000000001");
+
+    var state = false;
+    var switchService = new Service.Switch(config.name);
+
+    switchService
+        .getCharacteristic(Characteristic.On)
+        .on('set', function(value, callback) {
+            state = value;
+            if (state) {
+                switchOn();
+            } else {
+                switchOff();
+            }
+            callback();
+        });
+
+    switchService
+        .getCharacteristic(Characteristic.On)
+        .on('get', function(callback){
+            callback(null, state);
+        });
+
+    this.services = [ informationService, switchService ];
 }
 
 
 RadioSwitch.prototype = {
-  getServices : function (){
-    return this.services;
-  }
+    getServices : function (){
+        return this.services;
+    }
 }
